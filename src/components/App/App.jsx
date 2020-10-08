1586323c22db76198f5ce777e663de156845f926
Roman <roman@MacBook-Pro-Roman.local>
2020-10-08 19:13:10 +0300

    create and connect the component App to the project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<h1>It's the Form App!</h1>
+		</div>
+	);
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(console.log("It's Form App!"), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
