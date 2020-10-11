c8d96b831c203d1a9b50f766b67032d299407026
Roman <roman@MacBook-Pro-Roman.local>
2020-10-11 18:15:10 +0300

    Add styles for Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,31 +4,44 @@ import './Form.scss';
 const Form = () => {
 	return (
 		<form className="form">
-			<label>
-				Имя
-				<input className="form__input" type="text" name="name" />
-			</label>
+			<ul>
+				<li className="form__elem">
+					<label>
+						Имя
+						<input className="form__input" type="text" name="name" />
+					</label>
+				</li>
+				<li className="form__elem">
+					<label>
+						Фамилия
+						<input className="form__input" type="text" name="surname" />
+					</label>
+				</li>
+				<li className="form__elem">
+					<label>Дата рождения</label>
+					<input className="form__input" type="date" name="bithday" />
+				</li>
+				<li className="form__elem">
+					<div className="form__checkbox">
+						<span>Пол</span>
+						<label>
+							Муж
+							<input type="checkbox" name="phone" />
+						</label>
+						<label>
+							Жен
+							<input type="checkbox" name="phone" />
+						</label>
+					</div>
+				</li>
+				<li className="form__elem">
+					<label>
+						Телефон
+						<input className="form__input" type="tel" name="phone" />
+					</label>
+				</li>
+			</ul>
 
-			<label>
-				Фамилия
-				<input className="form__input" type="text" name="surname" />
-			</label>
-
-			<label>
-				Дата рождения
-				<input className="form__input" type="data" name="bithday" />
-			</label>
-			<label>
-				Пол
-				<select className="form__input">
-					<option>Муж</option>
-					<option>Жен</option>
-				</select>
-			</label>
-			<label>
-				Номер телефона
-				<input className="form__input" type="tel" name="phone" />
-			</label>
 			<button className="form__button" type="button">
 				Ввести
 			</button>
