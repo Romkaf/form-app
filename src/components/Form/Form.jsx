64403a27eec080cc0bbdcd7353c3ce2edbcabff7
Roman <roman@MacBook-Pro-Roman.local>
2020-10-16 23:31:37 +0300

    Add function of validation for form and change receiving values of form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,89 +4,78 @@ import { addDataToBlanc } from '../../models/actions';
 import './Form.scss';
 
 const Form = ({ addDataToBlanc }) => {
-	const [data, setData] = useState({
-		name: '',
-		surname: '',
-		birthday: '',
-		sex: '',
-		phone: '',
-	});
+	const handleValuesValidate = () => {
+		const form = document.forms['myForm'];
 
-	const handlerInputChange = (property) => (evt) => {
-		evt.persist();
-		setData((data) => ({
-			...data,
-			[property]: evt.target.value,
-		}));
+		const newData = {
+			name: form['name'].value,
+			surname: form['surname'].value,
+			birthday: form['birthday'].value,
+			sex: form['sex'].value,
+			phone: form['phone'].value,
+		};
+
+		const { name, surname, birthday, sex, phone } = newData;
+
+		if (name.match(/\d/g) || surname.match(/\d/g)) {
+			console.log('Используйте только буквы в имени и фамилии');
+		} else if (!name || !surname) {
+			console.log('Введите имя и фамилию');
+		}
+
+		if (!birthday) {
+			console.log('выберете дату');
+		}
+
+		if (!sex) {
+			console.log('выберете пол');
+		}
+
+		if (phone.match(/[^\d-]+/g)) {
+			console.log('Некорректный номер телефона');
+		} else if (!phone) {
+			console.log('Введите номер телефона');
+		}
+
+		console.log('newData', newData);
 	};
 
 	return (
-		<form className="form">
+		<form className="form" name="myForm">
 			<ul>
 				<li className="form__elem">
 					<label>
 						Имя
-						<input
-							className="form__input"
-							type="text"
-							name="name"
-							onChange={handlerInputChange('name')}
-						/>
+						<input className="form__input" type="text" name="name" />
 					</label>
 				</li>
 				<li className="form__elem">
 					<label>
 						Фамилия
-						<input
-							className="form__input"
-							type="text"
-							name="surname"
-							onChange={handlerInputChange('surname')}
-							required
-						/>
+						<input className="form__input" type="text" name="surname" />
 					</label>
 				</li>
 				<li className="form__elem">
 					<label>Дата рождения</label>
-					<input
-						className="form__input"
-						type="date"
-						name="bithday"
-						onChange={handlerInputChange('birthday')}
-					/>
+					<input className="form__input" type="date" name="birthday" />
 				</li>
 				<li className="form__elem">
 					<div className="form__checkbox">
 						<span>Пол</span>
 						<label>
 							Муж
-							<input
-								type="radio"
-								name="sex"
-								value="Муж"
-								onChange={handlerInputChange('sex')}
-							/>
+							<input type="radio" name="sex" value="Муж" />
 						</label>
 						<label>
 							Жен
-							<input
-								type="radio"
-								name="sex"
-								value="Жен"
-								onChange={handlerInputChange('sex')}
-							/>
+							<input type="radio" name="sex" value="Жен" />
 						</label>
 					</div>
 				</li>
 				<li className="form__elem">
 					<label>
 						Телефон
-						<input
-							className="form__input"
-							type="tel"
-							name="phone"
-							onChange={handlerInputChange('phone')}
-						/>
+						<input className="form__input" type="tel" name="phone" />
 					</label>
 				</li>
 			</ul>
@@ -94,7 +83,7 @@ const Form = ({ addDataToBlanc }) => {
 			<button
 				className="form__button"
 				type="button"
-				onClick={() => addDataToBlanc(data)}
+				onClick={handleValuesValidate}
 			>
 				Ввести
 			</button>
