6cae7a17bc526834682b7788e006263a5b3a6136
Roman <roman@MacBook-Pro-Roman.local>
2020-10-23 00:20:07 +0300

    Updates

---
+++ precache-manifest.d2686b547940e3131de1b331c003eb13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87f70ba2e27c1039122ae37de6c7428",
+    "url": "/form-app/index.html"
+  },
+  {
+    "revision": "34b0211821ca1c866ada",
+    "url": "/form-app/static/css/main.edd878b2.chunk.css"
+  },
+  {
+    "revision": "e69594dbbd57b1866f61",
+    "url": "/form-app/static/js/2.6beb02c3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/form-app/static/js/2.6beb02c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b0211821ca1c866ada",
+    "url": "/form-app/static/js/main.12b8e2df.chunk.js"
+  },
+  {
+    "revision": "e88ec66d77b16490f2c7",
+    "url": "/form-app/static/js/runtime-main.d6b20820.js"
+  },
+  {
+    "revision": "0f7fb3f830134f46529807a2baa3eb5e",
+    "url": "/form-app/static/media/roboto.0f7fb3f8.woff"
+  },
+  {
+    "revision": "400c14541b7297a097e57cc8f271f8a5",
+    "url": "/form-app/static/media/robotomedium.400c1454.woff2"
+  },
+  {
+    "revision": "f6be901fde0e25b6dd433e5a423ae955",
+    "url": "/form-app/static/media/robotomedium.f6be901f.woff"
+  }
+]);
